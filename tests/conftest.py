51744e304bf29c0d7d45789f264cc525f8ec10c2
Roberto Preste <roberto.preste@gmail.com>
2019-04-01 09:27:06 +0200

    Update tests and test files for list_datasets

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,10 +33,58 @@ def df_datasets_ensembl() -> pd.DataFrame:
 @pytest.fixture
 def df_datasets_mouse() -> pd.DataFrame:
     """
-    Dataframe with available datasets for the default mart
-    (ENSEMBL_MART_MOUSE).
+    Dataframe with available datasets for the ENSEMBL_MART_MOUSE mart.
     :return: pd.DataFrame
     """
     df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_mouse.pkl"))
     return df
 
+
+@pytest.fixture
+def df_datasets_sequence() -> pd.DataFrame:
+    """
+    Dataframe with available datasets for the ENSEMBL_MART_SEQUENCE mart.
+    :return: pd.DataFrame
+    """
+    df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_sequence.pkl"))
+    return df
+
+
+@pytest.fixture
+def df_datasets_ontology() -> pd.DataFrame:
+    """
+    Dataframe with available datasets for the ENSEMBL_MART_ONTOLOGY mart.
+    :return: pd.DataFrame
+    """
+    df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_ontology.pkl"))
+    return df
+
+
+@pytest.fixture
+def df_datasets_genomic() -> pd.DataFrame:
+    """
+    Dataframe with available datasets for the ENSEMBL_MART_GENOMIC mart.
+    :return: pd.DataFrame
+    """
+    df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_genomic.pkl"))
+    return df
+
+
+@pytest.fixture
+def df_datasets_snp() -> pd.DataFrame:
+    """
+    Dataframe with available datasets for the ENSEMBL_MART_SNP mart.
+    :return: pd.DataFrame
+    """
+    df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_snp.pkl"))
+    return df
+
+
+@pytest.fixture
+def df_datasets_funcgen() -> pd.DataFrame:
+    """
+    Dataframe with available datasets for the ENSEMBL_MART_FUNCGEN mart.
+    :return: pd.DataFrame
+    """
+    df = pd.read_pickle(os.path.join(DATADIR, "list_datasets_funcgen.pkl"))
+    return df
