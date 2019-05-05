f090ff8d175b9764f03b6476a1dd39388343f8b8
Roberto Preste <roberto.preste@gmail.com>
2019-05-05 10:44:28 +0200

    Bump version: 0.2.6 → 0.3.0

--- apybiomart/__init__.py
+++ apybiomart/__init__.py
@@ -6,4 +6,4 @@ from apybiomart.apybiomart import find_marts, find_datasets, find_attributes, \
 
 __author__ = """Roberto Preste"""
 __email__ = 'robertopreste@gmail.com'
-__version__ = '0.2.6'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(  # pragma: no cover
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/robertopreste/apybiomart',
-    version='0.2.6',
+    version='0.3.0',
     zip_safe=False,
 )
