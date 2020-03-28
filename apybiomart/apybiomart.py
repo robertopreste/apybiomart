2b87a880d9a63e095d0241fce4a03d0aaeedc91a
Roberto Preste <roberto.preste@gmail.com>
2020-03-28 18:55:22 +0000

    ENH: Add option to save output using a given filename

--- apybiomart/apybiomart.py
+++ apybiomart/apybiomart.py
@@ -7,56 +7,69 @@ from .classes import MartServer, DatasetServer, AttributesServer, \
     FiltersServer, Query
 
 
-def find_marts(save: bool = False) -> pd.DataFrame:
+def find_marts(save: bool = False,
+               output: str = "apybiomart_marts.csv") -> pd.DataFrame:
     """Retrieve and list available marts.
 
     Args:
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_marts.csv']
     """
-    server = MartServer(save=save)
+    server = MartServer(save=save, output=output)
     return server.find_marts()
 
 
 def find_datasets(mart: str = "ENSEMBL_MART_ENSEMBL",
-                  save: bool = False) -> pd.DataFrame:
+                  save: bool = False,
+                  output: str = "apybiomart_datasets.csv") -> pd.DataFrame:
     """Retrieve and list available datasets for a given mart.
 
     Args:
         mart: BioMart mart name (default: "ENSEMBL_MART_ENSEMBL")
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_datasets.csv']
     """
-    server = DatasetServer(mart, save=save)
+    server = DatasetServer(mart, save=save, output=output)
     return server.find_datasets()
 
 
 def find_attributes(dataset: str = "hsapiens_gene_ensembl",
-                    save: bool = False) -> pd.DataFrame:
+                    save: bool = False,
+                    output: str = "apybiomart_attributes.csv") -> pd.DataFrame:
     """Retrieve and list available attributes for a given mart.
 
     Args:
         dataset: BioMart dataset name (default: "hsapiens_gene_ensembl")
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_attributes.csv']
     """
-    server = AttributesServer(dataset, save=save)
+    server = AttributesServer(dataset, save=save, output=output)
     return server.find_attributes()
 
 
 def find_filters(dataset: str = "hsapiens_gene_ensembl",
-                 save: bool = False) -> pd.DataFrame:
+                 save: bool = False,
+                 output: str = "apybiomart_filters.csv") -> pd.DataFrame:
     """Retrieve and list available filters for a given mart.
 
     Args:
         dataset: BioMart dataset name (default: "hsapiens_gene_ensembl")
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_filters.csv']
     """
-    server = FiltersServer(dataset, save=save)
+    server = FiltersServer(dataset, save=save, output=output)
     return server.find_filters()
 
 
 def query(attributes: List[str],
           filters: Dict[str, Union[str, int, list, tuple, bool]],
           dataset: str = "hsapiens_gene_ensembl",
-          save: bool = False) -> pd.DataFrame:
+          save: bool = False,
+          output: str = "apybiomart_query.csv") -> pd.DataFrame:
     """Launch synchronous query using the given attributes, filters and dataset.
 
     Args:
@@ -64,15 +77,18 @@ def query(attributes: List[str],
         filters: dict of filter name : value to filter results
         dataset: BioMart dataset name (default: "hsapiens_gene_ensembl")
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_query.csv']
     """
-    server = Query(attributes, filters, dataset, save=save)
+    server = Query(attributes, filters, dataset, save=save, output=output)
     return server.query()
 
 
 async def aquery(attributes: List[str],
                  filters: Dict[str, Union[str, int, list, tuple, bool]],
                  dataset: str = "hsapiens_gene_ensembl",
-                 save: bool = False) -> pd.DataFrame:
+                 save: bool = False,
+                 output: str = "apybiomart_aquery.csv") -> pd.DataFrame:
     """Launch asynchronous query using the given attributes, filters and dataset.
 
     Args:
@@ -80,6 +96,8 @@ async def aquery(attributes: List[str],
         filters: dict of filter name : value to filter results
         dataset: BioMart dataset name (default: "hsapiens_gene_ensembl")
         save: save results to a CSV file [default: False]
+        output: output filename if saving results
+            [default: 'apybiomart_aquery.csv']
     """
-    server = Query(attributes, filters, dataset, save=save)
+    server = Query(attributes, filters, dataset, save=save, output=output)
     return await server.aquery()
--- apybiomart/commands/marts.py
+++ apybiomart/commands/marts.py
@@ -9,11 +9,13 @@ from apybiomart.apybiomart import find_marts
 
 @click.command("marts")
 @click.option("--save", "-s", default=False, is_flag=True,
-              help="Save results to a CSV file [default: False]")
-def cli_marts(save):
+              help="Save results to a CSV file", show_default=True)
+@click.option("--output", "-o", default="apybiomart_marts.csv", type=str,
+              help="Output filename if saving results", show_default=True)
+def cli_marts(save, output):
     """Retrieve and list available marts."""
     pd.set_option("max_rows", 999)
-    marts = find_marts(save=save)
+    marts = find_marts(save=save, output=output)
     marts.columns = [col.replace("_", " ") for col in marts.columns]
     click.echo(marts)
     return 0
